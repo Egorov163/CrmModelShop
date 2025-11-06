cf6e1ee46b9ce88d1b8f51abf72d97d6dcccc4cd
Aleksandr <Aleksandr_egorov_07@mail.ru>
2025-11-06 10:30:16 +0400

    Добавьте файлы проекта.

---
+++ CrmUi/Main.cs
@@ -0,0 +1,62 @@
+﻿using CrmBl.Model;
+using System;
+using System.Windows.Forms;
+
+namespace CrmUi
+{
+    public partial class Main : Form
+    {
+        CrmContext db;
+
+        public Main()
+        {
+            InitializeComponent();
+            db = new CrmContext();
+        }
+
+        private void сущностиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProductToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var catalogProduct = new Catalog<Product>(db.Products);
+            catalogProduct.Show();
+        }
+
+        private void SellerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var catalogSeller = new Catalog<Seller>(db.Sellers);
+            catalogSeller.Show();
+        }
+
+        private void CustomerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var catalogCustomer = new Catalog<Customer>(db.Customers);
+            catalogCustomer.Show();
+        }
+
+        private void CheckToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var catalogCheck = new Catalog<Check>(db.Checks);
+            catalogCheck.Show();
+        }
+
+        private void CustomerAddToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            var form = new CustomerForm();
+
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                db.Customers.Add(form.Customer);
+                db.SaveChanges();
+            }
+        }
+    }
+}
